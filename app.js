0dc740d2a8b8fd969da42b4acd4f8509ba6c6bb9
clockwork189 <charles.desouza3000@gmail.com>
2013-03-03 13:40:07 -0500

    Added initial functions

---
+++ app.js
@@ -0,0 +1,90 @@
+var GameOfLife = function () {
+    var self = {};
+
+    self.init = function () {
+        var grid = [];
+        for(var i = 0; i < 15; i++) {
+            var row = [];
+            for(var j = 0; j < 30; j++) {
+                var column = [0];
+                row.push(column);
+            }
+            grid.push(row);
+        }
+        return grid;
+    };
+
+    var getNumberOfNeighbors = function (matrix, i, j) {
+        var left = i - 1;
+        var right = i + 1;
+        var down = j - 1;
+        var up = j + 1;
+        var numAliveCells = 0;
+
+        if(check2DMatrix(matrix, i, up)) {
+            numAliveCells = addCount(numAliveCells, matrix[i][up]);
+        }
+        if(check2DMatrix(matrix, i, down)) {
+            numAliveCells = addCount(numAliveCells, matrix[i][down]);
+        }
+        if(check2DMatrix(matrix, right, j)) {
+            numAliveCells = addCount(numAliveCells, matrix[right][j]);
+        }
+        if(check2DMatrix(matrix, left, j)) {
+            numAliveCells = addCount(numAliveCells, matrix[left][j]);
+        }
+        if(check2DMatrix(matrix, left, up)) {
+            numAliveCells = addCount(numAliveCells, matrix[left][up]);
+        }
+        if(check2DMatrix(matrix, left, down)) {
+            numAliveCells = addCount(numAliveCells, matrix[left][down]);
+        }
+        if(check2DMatrix(matrix, right, up)) {
+            numAliveCells = addCount(numAliveCells, matrix[right][up]);
+        }
+        if(check2DMatrix(matrix, right, down)) {
+            numAliveCells = addCount(numAliveCells, matrix[right][down]);
+        }
+        return numAliveCells;
+    };
+
+    var check2DMatrix = function (matrix, i, j) {
+        if(matrix[i] !== undefined) {
+            if(matrix[i][j] !== undefined) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    var addCount = function (counter, val) {
+        if(val === 0) {
+            counter++;
+        }
+        return counter;
+    };
+
+    var getNextGen = function (matrix) {
+        var newMatrix = matrix;
+        for(var i = 0; i < matrix.length; i++) {
+            for(var j = 0; j < matrix[i].length; j++) {
+                if(check2DMatrix(matrix, i, j)) {
+                    var numNeighbors = getNumNeighbors(matrix, i , j);
+                    if(matrix[i][j] == 1) {
+                        if(numNeighbors < 2 || numNeighbors > 3) {
+                            newMatrix[i][j] = 0;
+                        }
+                    } else if(matrix[i][j] == 0) {
+                        if(numNeighbors == 3) {
+                            newMatrix[i][j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        //console.log(matrix);
+        return newMatrix;
+    };
+
+    return self;
+};
